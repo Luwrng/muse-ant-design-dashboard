11671b6df4c495e05402d8a5c2eb429189babbf9
Huynh Nguyen Thai Duong <115139619+hntduong12345@users.noreply.github.com>
2025-08-24 19:34:59 +0700

    fix: view gardener certificate when checking gardener registration

--- src/pages/Account/approveColumns.js
+++ src/pages/Account/approveColumns.js
@@ -8,7 +8,13 @@ import { faEye } from "@fortawesome/free-solid-svg-icons";
 import DetailButton from "../../components/button/DetailButton";
 import { cleanfood } from "../../api_admin";
 
-const updateRecordStatus = async (record, newStatus, setDataSource, fetchData, setActiveTab) => {
+const updateRecordStatus = async (
+  record,
+  newStatus,
+  setDataSource,
+  fetchData,
+  setActiveTab
+) => {
   try {
     // 👇 Truyền phẳng giá trị thay vì object
     await cleanfood.admin.updateAccountStatus(record.key, newStatus, true);
@@ -28,98 +34,101 @@ const updateRecordStatus = async (record, newStatus, setDataSource, fetchData, s
   }
 };
 
-
-
-
-const approveColumns = ({ handleViewImage,
+const approveColumns = ({
+  handleViewImage,
   openRejectModal,
   setDataSource,
   setActiveTab,
-  onApproved }) => [
-    {
-      title: "Hình đại diện",
-      dataIndex: "Avatar",
-      key: "avatar",
-      width: 80,
-      align: "center",
-      render: (avatar) => <Avatar size={40} src={avatar} />,
-    },
-    {
-      title: "Họ và tên",
-      dataIndex: "Name",
-      key: "name",
-      width: 150,
-      align: "center",
-    },
-    {
-      title: "Email",
-      dataIndex: "Email",
-      key: "email",
-      width: 200,
-      align: "center",
-    },
-    {
-      title: "Số điện thoại",
-      dataIndex: "PhoneNumber",
-      key: "phone",
-      width: 150,
-      align: "center",
-    },
-    {
-      title: "Xem chi tiết ",
-      dataIndex: "ViewCertificate",
-      key: "certificate",
-      width: 50,
-      align: "center",
-      render: (_, record) => (
-        <Space>
-          <DetailButton
-            onClick={() => handleViewImage(record.ViewCertificate)}
-            icon={faEye}
-            title="Xem chi tiết"
-          />
-        </Space>
-      ),
-    },
-    {
-      title: "Phê duyệt",
-      key: "actions",
-      width: 80,
-      align: "center",
-      render: (_, record) => (
-        <Space>
-          <FontAwesomeIcon
-            icon={faCheck}
-            onClick={async () => {
-              await updateRecordStatus(record, "ACTIVE", setDataSource, onApproved, setActiveTab);
-            }}
-
-            title="Duyệt"
-            style={{
-              fontSize: "16px",
-              color: "#52c41a",
-              cursor: "pointer",
-              transition: "all 0.3s",
-            }}
-            onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
-            onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
-          />
-          <FontAwesomeIcon
-            icon={faTimes}
-            onClick={() => openRejectModal(record)}
-            title="Không duyệt"
-            style={{
-              fontSize: "16px",
-              color: "#ff4d4f",
-              cursor: "pointer",
-              transition: "all 0.3s",
-            }}
-            onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
-            onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
-          />
-        </Space>
-      ),
-    }
-
-  ];
+  onApproved,
+}) => [
+  {
+    title: "Hình đại diện",
+    dataIndex: "Avatar",
+    key: "avatar",
+    width: 80,
+    align: "center",
+    render: (avatar) => <Avatar size={40} src={avatar} />,
+  },
+  {
+    title: "Họ và tên",
+    dataIndex: "Name",
+    key: "name",
+    width: 150,
+    align: "center",
+  },
+  {
+    title: "Email",
+    dataIndex: "Email",
+    key: "email",
+    width: 200,
+    align: "center",
+  },
+  {
+    title: "Số điện thoại",
+    dataIndex: "PhoneNumber",
+    key: "phone",
+    width: 150,
+    align: "center",
+  },
+  {
+    title: "Xem chi tiết ",
+    dataIndex: "ViewCertificate",
+    key: "certificate",
+    width: 50,
+    align: "center",
+    render: (_, record) => (
+      <Space>
+        <DetailButton
+          onClick={() => handleViewImage(record.CertificateImage)}
+          icon={faEye}
+          title="Xem chi tiết"
+        />
+      </Space>
+    ),
+  },
+  {
+    title: "Phê duyệt",
+    key: "actions",
+    width: 80,
+    align: "center",
+    render: (_, record) => (
+      <Space>
+        <FontAwesomeIcon
+          icon={faCheck}
+          onClick={async () => {
+            await updateRecordStatus(
+              record,
+              "ACTIVE",
+              setDataSource,
+              onApproved,
+              setActiveTab
+            );
+          }}
+          title="Duyệt"
+          style={{
+            fontSize: "16px",
+            color: "#52c41a",
+            cursor: "pointer",
+            transition: "all 0.3s",
+          }}
+          onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        />
+        <FontAwesomeIcon
+          icon={faTimes}
+          onClick={() => openRejectModal(record)}
+          title="Không duyệt"
+          style={{
+            fontSize: "16px",
+            color: "#ff4d4f",
+            cursor: "pointer",
+            transition: "all 0.3s",
+          }}
+          onMouseEnter={(e) => (e.currentTarget.style.transform = "scale(1.2)")}
+          onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
+        />
+      </Space>
+    ),
+  },
+];
 export default approveColumns;
